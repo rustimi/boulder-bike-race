ec3b70dde3529448ecbb037e23de06018848b420
rustimi <michele.rusticucci@gmail.com>
2024-01-26 00:06:55 +0100

    IMP home page timer

---
+++ app/javascript/controllers/countdown-timer.js
@@ -0,0 +1,30 @@
+(function() {
+    // Constants for time units
+    const second = 1000,
+        minute = second * 60,
+        hour = minute * 60,
+        day = hour * 24;
+
+    // Destination date
+    const destinationDate = new Date('04/01/2024').getTime();
+
+    // Update countdown every second
+    const countdownInterval = setInterval(updateCountdown, 500);
+
+    // Update countdown function
+    function updateCountdown() {
+        const now = new Date().getTime();
+        const distance = destinationDate - now;
+
+        // Update DOM elements
+        document.querySelector("#days").innerText = Math.floor(distance / day);
+        document.querySelector("#hours").innerText = Math.floor((distance % day) / hour);
+        document.querySelector("#minutes").innerText = Math.floor((distance % hour) / minute);
+        document.querySelector("#seconds").innerText = Math.floor((distance % minute) / second);
+
+        // Clear interval when countdown is complete
+        if (distance < 0) {
+            clearInterval(countdownInterval);
+        }
+    }
+})();
