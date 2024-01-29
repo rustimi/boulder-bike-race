92d2d4ea46446510367e97cfc8a21795e5b5367c
rustimi <michele.rusticucci@gmail.com>
2024-01-29 11:12:23 +0100

    implement basic flickr call

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,5 +4,8 @@ class PagesController < ApplicationController
   end
 
   def photos
+    flickr= Flickr.new "eec935e4108558f22596a8c810b943f3", "12b5c3d7178a1c69"
+    photos = flickr.photos.search(tags: "boulder")
+    @photo = Flickr.url(photos[0])
   end
 end
