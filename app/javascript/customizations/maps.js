acf4ae2b94d97ff457a4d6eafe2d62b7a71d1ba1
rustimi <michele.rusticucci@gmail.com>
2024-01-30 23:16:09 +0100

    made markers interactable with Rider model data

--- app/javascript/customizations/maps.js
+++ app/javascript/customizations/maps.js
@@ -1,55 +1,42 @@
-
 // Initialize and add the map
 let map;
 
 async function initMap() {
     // The location of Boulder
-    const position = { lat: 40.0187488, lng: -105.2607357 };
+    const position = {lat: 40.0187488, lng: -105.2607357};
 
-    const { Map, InfoWindow } = await google.maps.importLibrary("maps");
-    const { AdvancedMarkerElement} = await google.maps.importLibrary("marker");
+    const {Map, InfoWindow} = await google.maps.importLibrary("maps");
+    const {AdvancedMarkerElement} = await google.maps.importLibrary("marker");
 
     // The map, centered at Boulder
     map = new Map(document.querySelector("#map"), {
-        zoom: 12,
-        center: position,
-        mapId: "BOULDER_MAP",
+        zoom: 12, center: position, mapId: "BOULDER_MAP",
     });
+
+
+    const infoWindow = new InfoWindow();
 
     rider_info.forEach((rider) => {
         let rider_position = {lat: rider.lat, lng: rider.long};
-        new AdvancedMarkerElement({
-            map: map,
-            position: rider_position,
-            title: rider.fname,
+        let marker = new AdvancedMarkerElement({
+            map: map, position: rider_position, title: rider.fname,
+        });
+
+        marker.addListener("click", ({}) => {
+            infoWindow.close();
+            let infoCard = '' +
+                '<div class="intro-text left-0 text-center bg-faded p-5 rounded">\n' +
+                '        <h2 class="section-heading mb-4">\n' +
+                '          <span class="section-heading-upper">'+ rider.fname +' '+ rider.lname +'</span>\n' +
+                '        </h2>\n' +
+                '        <div class="intro-button mx-auto">\n' +
+                '          <a class="btn btn-primary" href="'+ rider.url+ '">Go to User!</a></div>\n' +
+                '      </div>'
+
+            infoWindow.setContent(infoCard);
+            infoWindow.open(marker.map, marker);
         });
     })
-    // The marker
-    // const marker = new AdvancedMarkerElement({
-    //     map: map,
-    //     position: position,
-    //     title: "Boulder",
-    // });
-    //
-    // const infoWindow = new InfoWindow();
-    // marker.addListener("click", ({}) => {
-    //
-    //     infoWindow.close();
-    //     let infoCard = '' +
-    //         '<div class="intro-text left-0 text-center bg-faded p-5 rounded">\n' +
-    //         '        <h2 class="section-heading mb-4">\n' +
-    //         '          <span class="section-heading-upper">What is about</span>\n' +
-    //         '        </h2>\n' +
-    //         '        <p class="mb-3">test123</p>\n' +
-    //         '        <div class="intro-button mx-auto">\n' +
-    //         '          <a class="btn btn-primary" href="#!">Go to User!</a></div>\n' +
-    //         '      </div>'
-    //
-    //     infoWindow.setContent(infoCard);
-    //     infoWindow.open(marker.map, marker);
-    // });
-
-
 }
 
 initMap();
