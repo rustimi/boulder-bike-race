5eefa1bd8c64b2015bef52d717e73d66e349407d
rustimi <michele.rusticucci@gmail.com>
2024-01-31 20:56:19 +0100

    IMP style and form for contest

--- app/controllers/contests_controller.rb
+++ app/controllers/contests_controller.rb
@@ -1,10 +1,21 @@
 class ContestsController < ApplicationController
   def new
-
+    @contest = Contest.new
   end
 
   def create
+    @contest = Contest.new(contest_params)
 
+    if @contest.save
+      # redirect_to @item
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
+
+  private
+  def contest_params
+    params.require(:contest).permit(:fname, :lname, :email, :slogan)
+  end
 end
