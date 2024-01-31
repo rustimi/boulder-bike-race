5dc9331a62edd8af6e6f2eda02d2efc1158e323e
rustimi <michele.rusticucci@gmail.com>
2024-01-31 22:37:27 +0100

    IMP basic validation

--- app/models/contest.rb
+++ app/models/contest.rb
@@ -1,2 +1,6 @@
 class Contest < ApplicationRecord
+  validates :fname, presence: true, length: { maximum: 25 }
+  validates :lname, presence: true, length: { maximum: 25 }
+  validates :email, presence: true, length: { maximum: 25 }, format: { with: URI::MailTo::EMAIL_REGEXP },   uniqueness: true
+  validates :slogan, presence: true, length: { maximum: 50 }
 end
