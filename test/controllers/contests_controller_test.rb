71d724ef6a51bc8d571dcb8d8d2c94e7e2a8136f
rustimi <michele.rusticucci@gmail.com>
2024-02-06 23:28:47 +0100

    added basic controller test

--- test/controllers/contests_controller_test.rb
+++ test/controllers/contests_controller_test.rb
@@ -1,7 +1,52 @@
 require "test_helper"
 
 class ContestsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user_1 = contests(:user_one)
+  end
+
+  test "should get new" do
+    get new_contest_url
+    assert_response :success
+  end
+
+  test "should create entry" do
+
+    params = {
+      contest: {
+        fname: "Mario",
+        lname: "Test",
+        email: "mario@test.com",
+        slogan: "Test slogan"
+      }
+    }
+    post contests_url, params: params
+    assert_redirected_to thankyou_url
+  end
+
+
+  test "should not create entry" do
+
+    params = {
+      contest: {
+        fname: "Mario",
+        lname: "Test",
+        email: "mario@test.com",
+      }
+    }
+
+    # missing slogan
+    post contests_url, params: params
+    assert_response 422
+
+    # wrong email format
+    params[:slogan] = "test"
+    params[:email] = "WrongFormat"
+    post contests_url, params: params
+    assert_response 422
+
+    
+  end
+
+
 end
